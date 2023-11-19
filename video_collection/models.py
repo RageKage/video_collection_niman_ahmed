f85ecaf3a800a388029e91655345fb6e65277f94
RageKage <99547837+RageKage@users.noreply.github.com>
2023-11-18 20:43:37 -0600

    Part 2 lab done, tests passed

--- video_collection/models.py
+++ video_collection/models.py
@@ -1,3 +1,40 @@
+from urllib import parse 
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+
+class Video(models.Model):
+    title = models.CharField(max_length=255)
+    url = models.CharField(max_length=400)
+    notes = models.TextField(blank=True, null=True) # optional field
+    video_ID= models.CharField(max_length=40, unique=True) 
+    
+    def save(self, *args, **kwargs):
+        if not self.video_ID:
+            if self.url.startswith('https://www.youtube.com/watch?v'): # checks if the url is a youtube video
+                if '=' in self.url:
+                    # splits the url by = and takes the first index, outputs ['https://www.youtube.com/watch?v', 'HNHu-beUzZM&list', 'WL&index', '5']
+
+                    video_id_part = self.url.split('=')[1].split('&')[0]
+                    # Check if the video_id_part is of a typical YouTube ID length
+                    if len(video_id_part) != 11: # checks if the video_id_part is 11 characters long
+                        raise ValidationError('Invalid URL, must be a YouTube video.')
+                    self.video_ID = video_id_part
+                else:
+                    raise ValidationError('Invalid URL, must be a YouTube video.')
+            else:
+                raise ValidationError('Invalid URL, must be a YouTube video.')
+            
+        else:
+            # error when video_ID is not unique
+            raise ValidationError('Video ID already exists')
+        super().save(*args, **kwargs)
+
+
+
+
+
+    
+    def __str__(self):
+        return f'ID: {self.id} - Name: {self.title} - URL: {self.url} - Notes: {self.notes[:200]} - Video_ID: {self.video_ID}' # chuck the notes to the first 200 characters
--- video_collection/views.py
+++ video_collection/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import *
+from .models import Video
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from django.db.models.functions import Lower
+
 
 # Create your views here.
+
+
+def home(request):
+    app_name = "Exercise Videos!"
+    
+    return render(request, 'video_collection/home.html', {'app_name': app_name})
+
+
+
+def add_video(request):
+    if request.method == 'POST': 
+        form = VideoForm(request.POST) # form with the data from POST request
+        if form.is_valid():
+            try:
+                form.save() # save the video
+                messages.info(request, 'Video added successfully!')
+                return redirect('video_list') # redirect to video_list page
+            except ValidationError: # if the url is not a youtube video
+                messages.warning(request, 'Invalid URL, must be a YouTube video.')
+            except IntegrityError: # if the video_ID is not unique
+                messages.warning(request, 'Video ID already exists.')
+        else:
+            messages.warning(request, 'Invalid form, please try again.')
+            return render(request, 'video_collection/add_video.html', {'new_video_form': form})
+
+
+    form = VideoForm() 
+    return render(request, 'video_collection/add_video.html', {'new_video_form': form})
+
+
+def video_list(request):
+    
+    search_form = SearchForm(request.GET) # get the search term from the form
+    
+    if search_form.is_valid():
+        search_term = search_form.cleaned_data['search_term']
+        videos = Video.objects.filter(title__icontains=search_term).order_by(Lower('title')) # get videos that contain the search term
+    
+    else:
+        search_term = SearchForm()
+        videos = Video.objects.all().order_by(Lower('title')) 
+    return render(request, 'video_collection/video_list.html', {'videos': videos, 'search_form': search_form, 'search_term': search_term})
